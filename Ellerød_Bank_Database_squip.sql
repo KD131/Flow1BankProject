ee62ccfa6bebde745e9759c6e0a7feaf8bb05b5d
Dosei-desu <71060306+Dosei-desu@users.noreply.github.com>
2021-02-16 12:39:09 +0100

    database stuff

--- Ellerød_Bank_Database_squip.sql
+++ Ellerød_Bank_Database_squip.sql
@@ -9,8 +9,12 @@ CREATE TABLE accounts(
 	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 -- employee (0 = "no" | 1 = "yes")
 	employee boolean NOT NULL DEFAULT 0,
+-- bank_id
+	bank_id int NOT NULL DEFAULT -1,
 -- name
 	`name` varchar(80) NOT NULL,
+-- city
+	city varchar(80) NOT NULL DEFAULT "Blank",
 -- account balance
     balance int NOT NULL  DEFAULT 0,
 -- username
@@ -21,7 +25,8 @@ CREATE TABLE accounts(
 );
 */
 
--- INSERT INTO accounts (employee, `name`, username, `password`) VALUES (1, "Kris", "kris12", "wasspord");
--- INSERT INTO accounts (`name`, balance) VALUES ("Test User 1", "4007");
+INSERT INTO accounts (employee, `name`, city, username, `password`) VALUES (1, "Kris", "B-Town", "kris12", "wasspord");
+INSERT INTO accounts (bank_id, `name`, city, balance) VALUES ("60982341", "Test User 1", "K-Town", "4007");
+INSERT INTO accounts (bank_id, `name`, balance) VALUES ("98321732", "Test User 2", "11372");
 
 SELECT * FROM accounts;
